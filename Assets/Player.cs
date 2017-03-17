c23ed8cc78b890b565b2c23c7c98f1cf149d1b4d
Daniel Schukies <daniel@schukies.io>
2017-03-17 23:46:58 +0100

    add player eat

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float PlayerSize = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "food") 
+		{
+			col.gameObject.GetComponent<Food> ().respawn ();
+			PlayerSize += 0.1f * col.gameObject.GetComponent<Food> ().eatSize;
+			this.Scale ();
+		}
+	}
+
+	public void Scale()
+	{
+		transform.localScale += new Vector3(PlayerSize * transform.localScale.x, PlayerSize * transform.localScale.y, PlayerSize * transform.localScale.x);
+	}
+}
--- Assets/Scriptes/Food.cs
+++ Assets/Scriptes/Food.cs
@@ -5,6 +5,7 @@ public class Food : MonoBehaviour
 {
 
 	private Vector3 startPosition;
+	public float eatSize = 1;
 
 	// Use this for initialization
 	void Start () 
@@ -18,6 +19,16 @@ public class Food : MonoBehaviour
 	
 	}
 
+	void OnCollisonEnter2D(Collision2D col)
+	{
+		Debug.Log ("Destroy!!22");
+		if (col.gameObject.tag == "Player") 
+		{
+			Debug.Log ("Destroy!!");
+			this.respawn ();
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "deadzone") 
@@ -26,4 +37,11 @@ public class Food : MonoBehaviour
 			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
 		}
 	}
+
+	public void respawn()
+	{
+		Destroy (gameObject);
+	}
+
+
 }
