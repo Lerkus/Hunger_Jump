e2a99ce87b12bd2c57938d6939c9c5402301ea74
Daniel Schukies <daniel@schukies.io>
2017-03-17 22:24:23 +0100

    add player and deadzone + food

---
+++ Assets/Food.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour 
+{
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "deadzone") 
+		{
+			transform.position = startPosition;
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		}
+	}
+}
