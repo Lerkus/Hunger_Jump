68acbdbf2d7458e0f48101b3b3c8e59474b37248
Lerkus <Sebastian-Maag@gmx.de>
2017-03-17 22:23:00 +0100

    spawner script prototype implemented

--- Assets/Scriptes/ObjectMoveClient.cs
+++ Assets/Scriptes/ObjectMoveClient.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class ObjectMoveClient : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    
+
+
+
+
+    public void updateVelocity()
+    {
+
+    }
 }
---
+++ Assets/Scriptes/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject cloudFast;
+    public float cloudSpawnCycleTime = 0.1f;
+    public GameObject cloudSlow;
+    public float fallingObjectsSpawnCycleTime = 0.1f;
+
+    public GameObject masterParent;
+
+    public Vector2 spawnRangeLeftPoint = new Vector2();
+    public Vector2 spawnRangeRightPoint = new Vector2();
+
+    public GameObject[] fallingObjectsPrefabs;
+
+    private Coroutine cloudSpawnRoutine;
+    private Coroutine fallingObjectsRoutine;
+
+    public void Start()
+    {
+        cloudSpawnRoutine = StartCoroutine(spawnClouds());
+        fallingObjectsRoutine = StartCoroutine(spawnFallingObjects());
+    }
+
+    public IEnumerator spawnClouds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cloudSpawnCycleTime);
+            spawnObject(cloudFast);
+            spawnObject(cloudSlow);
+        }
+    }
+
+    public IEnumerator spawnFallingObjects()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(fallingObjectsSpawnCycleTime);
+            spawnObject(fallingObjectsPrefabs[Random.Range(0, fallingObjectsPrefabs.Length)]);
+        }
+    }
+
+    private void spawnObject(GameObject prefabToSpawn)
+    {
+        GameObject spawned = (GameObject) Instantiate(prefabToSpawn, Vector2.Lerp(spawnRangeLeftPoint, spawnRangeRightPoint, Random.Range(0f,1f)) , new Quaternion());
+        spawned.transform.parent = masterParent.transform;
+    }
+}
