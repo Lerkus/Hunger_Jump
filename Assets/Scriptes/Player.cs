1ba170e29255fcf41a3f3b54ffbf259e87f8cbff
Daniel Schukies <daniel@schukies.io>
2017-03-18 00:43:42 +0100

    add camera zoom and player growth

--- Assets/Scriptes/Player.cs
+++ Assets/Scriptes/Player.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Player : MonoBehaviour 
 {
 	public float PlayerSize = 1;
+	public float GrowRate = 1;
+	public GameObject Camera;
 
 	// Use this for initialization
 	void Start () {
@@ -23,11 +25,18 @@ public class Player : MonoBehaviour
 			col.gameObject.GetComponent<Food> ().respawn ();
 			PlayerSize += 0.1f * col.gameObject.GetComponent<Food> ().eatSize;
 			this.Scale ();
+			this.ScaleCamera ();
 		}
 	}
 
 	public void Scale()
 	{
-		transform.localScale += new Vector3(PlayerSize * transform.localScale.x, PlayerSize * transform.localScale.y, PlayerSize * transform.localScale.x);
+		transform.localScale += new Vector3(PlayerSize * GrowRate, PlayerSize * GrowRate, PlayerSize *  GrowRate);
+	}
+
+	private void ScaleCamera()
+	{
+		Camera.GetComponent<Camera> ().orthographicSize += PlayerSize * GrowRate * 10;
+		//Camera.transform.localScale += new Vector3(PlayerSize * GrowRate, PlayerSize * GrowRate, PlayerSize *  GrowRate);
 	}
 }
