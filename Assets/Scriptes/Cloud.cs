207562b5355883248b6284b78aad9c1695853071
Lerkus <Sebastian-Maag@gmx.de>
2017-03-18 13:46:22 +0100

    now implemented clouds

---
+++ Assets/Scriptes/Cloud.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour
+{
+
+    public SpriteRenderer[] possibleCloudsToShow;
+    public float randomSizeRange = 0.5f;
+
+    void Start()
+    {
+        int randomIndex = Random.Range(0, possibleCloudsToShow.Length);
+        possibleCloudsToShow[randomIndex].enabled = true;
+        possibleCloudsToShow[randomIndex].gameObject.transform.localScale *= 1 - Random.Range(-randomSizeRange, randomSizeRange);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
